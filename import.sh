33d8850e7ac1d0fd4fe8482b8800ac5208320240
Eric Martin <eric@ericmmartin.com>
2018-06-27 10:29:46 -0700

    Force import to ensure keyspace is correct in the sql file.

--- import.sh
+++ import.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 -f <tar_file> [-h <host>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 -f <tar_file> -k <keyspace> [-h <host>]" 1>&2; exit 1; }
 
 host=localhost
 
-while getopts ":f:h:" o; do
+while getopts ":f:k:h:" o; do
     case "${o}" in
         f)
             tar_file=${OPTARG}
+            ;;
+        k)
+            keyspace=${OPTARG}
             ;;
         h)
             host=${OPTARG}
@@ -19,12 +22,12 @@ while getopts ":f:h:" o; do
 done
 shift $((OPTIND-1))
 
-if [ -z "${tar_file}" ]; then
+if [ -z "${tar_file}" ] || [ -z "${keyspace}" ]; then
     usage
 fi
 
 bkp_name="bkp-$$"
-keyspace=$(basename "${tar_file}" ".tar.gz")
+data_dir=$(basename "${tar_file}" ".tar.gz")
 
 mkdir -p "${bkp_name}"
 
@@ -32,14 +35,17 @@ tar -xvzf "${tar_file}" -C "${bkp_name}"
 
 # update sql file with keyspace settings from host system
 keyspace_sql=$(cqlsh "${host}" -e "desc \"${keyspace}\";" | grep "CREATE KEYSPACE")
-sed -i "s/CREATE KEYSPACE .*/${keyspace_sql}/" "${bkp_name}/${keyspace}.sql"
+sed -i "s/CREATE KEYSPACE .*/${keyspace_sql}/" "${bkp_name}/${data_dir}.sql"
+
+# make sure the keyspace name are correct
+sed -i "s/CREATE TABLE [A-Za-z]\{1,\}./CREATE TABLE ${keyspace}./" "${bkp_name}/${data_dir}.sql"
 
 echo "Drop keyspace ${keyspace}"
 cqlsh --request-timeout="60" -e "drop keyspace \"${keyspace}\";"
 
 echo "Create empty keyspace: ${keyspace}"
-cat "${bkp_name}/${keyspace}.sql" | cqlsh
+cat "${bkp_name}/${data_dir}.sql" | cqlsh
 
-for dir in "${bkp_name}/${keyspace}/"*; do
+for dir in "${bkp_name}/${data_dir}/"*; do
     sstableloader -d "${host}" "${dir}"
 done
